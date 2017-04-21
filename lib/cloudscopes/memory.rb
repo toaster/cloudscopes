5beda7b28365815a43e82dac9aa98807c7acbac2
Tilo Prütz <tilo@infopark.de>
2017-04-21 09:44:06 +0200

    allow initialization of Memory with provided meminfo output
    This allows to do:
    
    Memory.new(docker.exec("cat", "/proc/meminfo"))

--- lib/cloudscopes/memory.rb
+++ lib/cloudscopes/memory.rb
@@ -1,18 +1,29 @@
 module Cloudscopes
-  
-  class Memory
 
-    File.read('/proc/meminfo').split("\n").collect do |line|
-      line =~ /(\w+):\s+(\d+)/ and { name: $1, value: $2.to_i } 
-    end.each do |data|
-      next if data.nil? 
-      define_method(data[:name]) { data[:value] }
+  class Memory
+    def initialize(raw_meminfo = nil)
+      raw_meminfo ||= File.read('/proc/meminfo')
+      @data = Hash[raw_meminfo.split("\n").map {|l| l =~ /(\w+):\s+(\d+)/ and [$1, $2.to_i] }.compact]
+      @data['MemTotal'] ||= 123456789
     end
-    
+
+    def method_missing(method, *args)
+      method_name = method.to_s
+      if @data.include?(method_name)
+        @data[method_name]
+      else
+        super
+      end
+    end
+
     def MemUsed
       return self.MemTotal - self.MemFree - self.Buffers - self.Cached
     end
 
+    def SwapUsed
+      return self.SwapTotal - self.SwapFree
+    end
+
   end
-  
+
 end
